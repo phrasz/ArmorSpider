27d53bc19834d044d963bd73fc7ec03fb26f8e4a
phrasz <phrasz@github.com>
2013-06-14 11:38:44 -0400

    Arbeiten Parser, Gut und Schlecht Liste Liste Work Versuch, Bildwandler hinzuzufügen.

--- ArmorSpider.sh
+++ ArmorSpider.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
-clear
 
-echo "   +-----------------------------------------"
-echo "   :\". /  /  /"
-echo "   :.-\". /  /"
-echo "   : _.-\". /"
-echo "   :\"  _.-\"."
-echo "   :-\"\"     \"."
-echo "   :"
-echo "   :"
-echo " ^.-.^"
-echo "\'^\\+/^\`"
-echo "'/`\"'\\`"
-
-GermanWordlist=("Breite" "Herstellung" "Jagd" "Orient" "Gewicht" "Hieb" "Stichwaffen") #"cm"
-GermanBADlist=("Foto" "Druck" "Druckgraphik" "Fotografie" "Fotopapier") #"cm"
+##########################################################
+# Configuration:
+# --------------
+GermanWordlist=("Breite" "Herstellung" "Jagd" "Orient" "Gewicht" "Hieb" "Stichwaffen" "cm")
+GermanBADlist=("Foto" "Druck" "Druckschriften" "Druckgraphik" "Fotografie" "Fotopapier" "Tischuhr")
 
 SavedListToLookAt="BockusAwesomeLinks.txt"
+
+Counter_BadPages=0
+Counter_GoodPages=0
+Counter_PagesSithKeyWords=0
+
+#Would you like to see debug messages?
+debug=false
+
 rm $SavedListToLookAt
+
+amount=`find . | grep -v ".git" | wc -l`
+echo "[ArmorSpider] Parsing $amount File(s) for keywords"
+#Now look through ALL the files downloaded, and save web pages to a list
+
+counter=1
+ItemCounter=0
+echo "`find . | grep -v \".git\"`" > ListOfFiles.txt
+ItemsToScan=`find .|wc -l`
+
+x="lolololololol"
+##########################################################
+
+##########################################################
+# Print Splash Screen:
+# --------------------
+clear
+echo "   +-----------------------------------------"
+echo "   :\". /  /  /"
+echo "   :.-\". /  /         ARMOR SPIDER"
+echo "   : _.-\". /             v 1.0"
+echo "   :\"  _.-\"."
+echo "   :-\"\"     \"."
+echo "   :                        By:"
+echo "   :                   Phrasz - 2013"
+echo " ^.-.^"
+echo "'^\\+/^\`"
+echo "'/\`\"'\\\`"
 
 echo "[ArmorSpider] Super Awesome Armor Finder..."
 echo ""
@@ -26,20 +52,25 @@ echo "[ArmorSpider] Looking for ${#GermanWordlist[@]} words"
 echo "[ArmorSpider] Banned ${#GermanBADlist[@]} words"
 echo "[ArmorSpider] Press enter to continue..."
 read pressentertocontinue
+##########################################################
 
-amount=`find . | wc -l`
-echo "[ArmorSpider] Parsing $amount File(s) for keywords"
-#Now look through ALL the files downloaded, and save web pages to a list
 
-counter=1
-ItemCounter=0
-echo "`find .`" > ListOfFiles.txt
-ItemsToScan=`find .|wc -l`
 
-x="lolololololol"
+##########################################################
+# Spider Logic:
+# -------------
 #Look for page title within ALL Pages with Keywords
 while read WebPage; do
 	clear
+	echo " ^.-.^"
+	echo "'^\\+/^\`      Crawling ..."
+	echo "'/\`\"'\\\`"
+	echo "[ArmorSpider] Current Stats:"
+	echo "[ArmorSpider] Bad: $Counter_BadPages"
+	echo "[ArmorSpider] Good: $Counter_GoodPages"
+	echo "[ArmorSpider] Keyword Pages: $Counter_PagesSithKeyWords"
+	echo ""
+
 	echo "[$counter / $ItemsToScan] Looking at $WebPage"
 	Title=`cat "$WebPage" | grep "<class=\"dbtitle\">"` #Pull the title if it has one
 	if [ "$x"!="$x$Title" ]; then #IF Title is NOT empty
@@ -52,25 +83,35 @@ while read WebPage; do
 	for SearchWord in "${GermanBADlist[@]}"
 	do
 		if $notBad; then
-			echo "[ArmorSpider] Searching for BAD word: $SearchWord"
+			if $debug; then
+				echo "[ArmorSpider] Searching for BAD word: $SearchWord"
+			fi
 			tempFinding=`cat "$WebPage" | grep "$SearchWord"`
 			if [ "$x$tempFinding" != "$x" ]; then
 				echo "Found: ${GermanBADlist[@]} in $WebPage"
-				echo "$WebPage" >> $SavedListToLookAt
+				#echo "$WebPage" >> $SavedListToLookAt
+				Counter_BadPages=$((Counter_BadPages + 1))
 				notBad=false
 			fi
 		fi
 	done
 
+
 	if $notBad; then
+		Counter_GoodPages=$((Counter_GoodPages + 1))
+
 		for SearchWord in "${GermanWordlist[@]}"
 		do
+
 			if $notfound; then
-				echo "[ArmorSpider] Searching for word: $SearchWord"
+				if $debug; then
+					echo "[ArmorSpider] Searching for word: $SearchWord"
+				fi
 				tempFinding=`cat "$WebPage" | grep "$SearchWord"`
 				if [ "$x$tempFinding" != "$x" ]; then
 					echo "Found: ${GermanWordlist[@]} in $WebPage"
 					echo "$WebPage" >> $SavedListToLookAt
+					Counter_PagesSithKeyWords=$((Counter_PagesSithKeyWords + 1))
 					notfound=false
 				fi
 				ItemCounter=$(( ItemCounter + 1))
@@ -82,4 +123,30 @@ while read WebPage; do
 done < ListOfFiles.txt
 
 rm ListOfFiles.txt
+##########################################################
+clear
 
+echo "   +-----------------------------------------"
+echo "   :\". /  /  /"
+echo "   :.-\". /  /         ARMOR SPIDER"
+echo "   : _.-\". /             v 1.0"
+echo "   :\"  _.-\"."
+echo "   :-\"\"     \"."
+echo "   :                        By:"
+echo "   :                   Phrasz - 2013"
+echo " ^.-.^"
+echo "'^\\+/^\`"
+echo "'/\`\"'\\\`"
+echo ""
+echo "[ArmorSpider] Scanning Completed\! Final Stats:"
+echo "[ArmorSpider] Bad: $Counter_BadPages"
+echo "[ArmorSpider] Good: $Counter_GoodPages"
+echo "[ArmorSpider] Keyword Pages: $Counter_PagesSithKeyWords"
+Convert="Y"
+echo ""
+echo "[ArmorSpider] Would you like to attempt images to webpage conversion? [Y/n]"
+read ConvertInput
+
+if[ "$Convert" == "$Convert$ConvertInput" ]; then
+	echo "[ArmorSpider] Attempting Conversion..."
+fi
